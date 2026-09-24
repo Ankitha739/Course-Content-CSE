2bb1ce3b9b1b2fcf7e86c0df7fea63fdcfec6ae1
Ankitha <ankithaaila@gmail.com>
2026-09-24 13:16:15 +0530

    server added

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,8 @@
-const mongoose =  require("mongoose")
+const mongoose = require("mongoose");
 
-async function connectDB(){
-    await mongoose.connect(process.env.MONGO_URL)
-    console.log("MongoDB connected successfully...")
+async function connectDB() {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB connected successfully...");
 }
 
-module.exports = connectDB
+module.exports = connectDB;
